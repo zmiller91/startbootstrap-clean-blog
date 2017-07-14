8eb86ec0727609150135e96031647c5301912ca6
Zachary Miller <2.4.8.16.32.sixtyfour@gmail.com>
2017-07-14 02:07:36 -0600

    Added a set method on the ContentData service to conveniently set urls

--- js/zackmiller.js
+++ js/zackmiller.js
@@ -1,71 +1,67 @@
+var app = angular.module('zackmiller', ['ngRoute']);
 
-    console.log("hi");
-    
-    var app = angular.module('zackmiller', ['ngRoute']);
-    
-    app.controller('HeaderCtrl', function($scope, ContentData) {
-        $scope.background = ContentData.background;
-    });
-    
-    app.directive('navigation', [function() {
-        return {
-            templateUrl: "html/navigation.html"
+app.controller('HeaderCtrl', function($scope, ContentData) {
+    $scope.background = ContentData.background;
+});
+
+app.directive('navigation', [function() {
+    return {
+        templateUrl: "html/navigation.html"
+    };
+}]);
+
+app.directive('zmHeader', ['ContentData', function(ContentData) {
+    return {
+        templateUrl: function() {
+            return ContentData.header;
+        }
+    };
+}]);
+
+app.directive('zmContent', ['ContentData', function(ContentData) {
+    return {
+        templateUrl: function() {
+            return ContentData.article;
+        }
+    };
+}]);
+
+app.directive('zmFooter', [function() {
+    return {
+        templateUrl: "html/footer.html"
+    };
+}]);
+
+app.service('ContentData', [function() {
+
+        this.set = function(content) {
+            this.header = 'html/header/' + content + '.html';
+            this.article = 'html/content/' + content + '.html';
+            this.background = 'img/' + content + '.jpg';
         };
-    }]);
-    
-    app.directive('zmHeader', ['ContentData', function(ContentData) {
-        return {
-            templateUrl: function() {
-                return ContentData.header;
+
+        this.set('homepage');
+}]);
+
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+
+        .when('/lawncron', {
+            templateUrl: 'html/site.html',
+            resolve: {
+                init: ['ContentData', function(ContentData) {
+                    ContentData.set('lawncron');
+                }]
             }
-        };
-    }]);
-    
-    app.directive('zmContent', ['ContentData', function(ContentData) {
-        return {
-            templateUrl: function() {
-                return ContentData.article;
+        })
+
+        .otherwise({
+            templateUrl: 'html/site.html',
+            resolve: {
+                init: ['ContentData', function(ContentData) {
+                    ContentData.set('homepage');
+                }]
             }
-        };
-    }]);
-    
-    app.directive('zmFooter', [function() {
-        return {
-            templateUrl: "html/footer.html"
-        };
-    }]);
-    
-    app.service('ContentData', [function() {
-            this.header = 'html/header/homepage.html';
-            this.article = 'html/content/homepage.html';
-            this.background = 'img/home-bg.jpg';
-    }]);
-    
-    app.config(function($routeProvider, $locationProvider) {
-        
-        $routeProvider
-            .when('/lawncron', {
-                templateUrl: 'html/site.html',
-                resolve: {
-                    init: ['ContentData', function(ContentData) {
-                        ContentData.header = "html/header/lawncron.html";
-                        ContentData.article = "html/content/lawncron.html";
-                        ContentData.background = 'img/about-bg.jpg';
-                    }]
-                }
-            })
-
-            .otherwise({
-                templateUrl: 'html/site.html',
-                resolve: {
-                    init: ['ContentData', function(ContentData) {
-                        ContentData.header = "html/header/homepage.html";
-                        ContentData.article = "html/content/homepage.html";
-                        ContentData.background = 'img/home-bg.jpg';
-                    }]
-                }
-            });
-
-        $locationProvider.html5Mode(true);
-    });
-    
+        });
+});
